7d5793a9f2966dfc25b10654332b4198afd8963d
Chris Crowell <ccrowell18@gmail.com>
2018-05-06 19:56:44 -0400

    Add player delete handling

--- src/routes/api/players.js
+++ src/routes/api/players.js
@@ -51,8 +51,25 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.delete('/:playerId', function(req, res) {
-
+router.delete('/:playerId', function(req, res, next) {
+  passport.authenticate('jwt', {session: false}, function(err, user, info) {
+    if(!user) {
+      return res.status(403).send('not authenticated');
+    } else {
+      let userId = user._id;
+      let playerId = req.params.playerId;
+      Player.findOne({_id: playerId}, (err, player) => {
+        if(err || !player) {
+          return res.status(404).send();
+        } else if(userId != player.created_by) {
+          return res.status(404).send();
+        } else {
+          player.remove();
+          return res.status(200).send();
+        }
+      });
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
